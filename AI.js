8214b33d64c73cd83eb9fea952ee90d27e4d393e
rosasynstylae <64bitsuperhero@gmail.com>
2015-09-08 16:02:12 -0600

    added AI.js

---
+++ AI.js
@@ -0,0 +1,26 @@
+/* A Cleverbot App, meant to pass the Turing Test
+ * 
+ * For CS427
+ *
+ * Written by Katelyn Narum, Dan Green and Peter Ortegel
+ */
+
+document.onload=function() {
+	//I'm guessing we're going to want to bind enter
+}
+
+
+
+/* reply
+ * This function controls the overall flow for the cleverbot
+ * 
+ * Takes: nothing
+ * Returns: Output
+ */
+function reply() {
+	//get input
+	//match complete phrases
+	//match partial phrases
+	//parse
+	//return output
+}
