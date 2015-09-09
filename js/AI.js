b07902689327b3f86f6626b5bbccebc621827f5f
rosasynstylae <64bitsuperhero@gmail.com>
2015-09-09 13:04:48 -0600

    AI.js now handles input and output - just needs the Database and Parsing

--- js/AI.js
+++ js/AI.js
@@ -5,9 +5,10 @@
  * Written by Katelyn Narum, Dan Green and Peter Ortegel
  */
 
-document.onload=function() {
-	//I'm guessing we're going to want to bind enter
-}
+/* Binds Enter to call reply */
+$('#usertext').keypress(function(event){
+	if(event.keyCode == '13') { reply(); }
+});
 
 
 
@@ -19,8 +20,21 @@ document.onload=function() {
  */
 function reply() {
 	//get input
+	var input = $('#usertext').val();
+	//set input into html page
+	$('#container').append('<div class="user-content"><h3 class="user-text col-xs-12">' 
+							+ input + '</h3></div>');
+	//scroll to bottom
+	$('#container').scrollTop($("#container")[0].scrollHeight);
+	
 	//match complete phrases
 	//match partial phrases
 	//parse
+	
 	//return output
+	var output = "boo";
+	$('#container').append('<div class="ai-content">'
+			+ '<h3 class="ai-text col-xs-12 text-right">' + output + '</h3></div>');
+	//scroll to bottom
+	$('#container').scrollTop($("#container")[0].scrollHeight);
 }
